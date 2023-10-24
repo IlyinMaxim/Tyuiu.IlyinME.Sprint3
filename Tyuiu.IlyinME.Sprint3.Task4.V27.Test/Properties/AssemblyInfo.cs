65dff0dd89264beb69edb7e3c91b32618d9c1820
Tyuiu.IlyinME@outlook.com <maxchance05@mail.ru>
2023-10-24 17:00:35 +0500

    Task4

---
+++ Tyuiu.IlyinME.Sprint3.Task4.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IlyinME.Sprint3.Task4.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IlyinME.Sprint3.Task4.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c909bce-84a1-4efb-afad-48bdb42bea63")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
